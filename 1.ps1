1e3567a729ed8045b1beeddd4d14972439c9d7fb
Cheng Huang <chuang@mdsol.com>
2022-04-22 14:30:10 -0400

    test

--- 1.ps1
+++ 1.ps1
@@ -1,3 +1,6 @@
-Write-Host "1.ps1"
-Write-Host "::error file=1.ps1::Error is 1.ps1"
+Write-Host "::group::Errors in 1.ps1"
+Write-Host "::error file=1.ps1::Error 1"
+Write-Host "::error file=1.ps1::Error 2"
+Write-Host "::error file=1.ps1::Error 3"
+Write-Host "::endgroup::"
 exit 1
