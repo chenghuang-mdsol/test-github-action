74acc38f0e71d7b8998c2d4292ff06c234a4b8e6
Cheng Huang <chuang@mdsol.com>
2023-08-15 15:49:15 -0400

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,6 +1,1 @@
-Write-Host "::group::Errors in 1.ps1"
-Write-Host "::error file=1.ps1::Error 1"
-Write-Host "::error file=1.ps1::Error 2"
-Write-Host "::error file=1.ps1::Error 3"
-Write-Host "::endgroup::"
-exit 1
+Write-Host "567"
